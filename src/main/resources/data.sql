86e56a2b4245d34644ed0a6e8be01e27f7da8aa9
Stanislaw <sta.glegola@gmail.com>
2019-09-04 23:20:12 +0200

    database sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into Book (id, author, title) values (nextval('book_seq'),'Tomasz Kaczanowski','Testy');
+insert into Book (id, author, title) values (nextval('book_seq'),'Henryk Sienkiewicz','Potop');
+insert into Book (id, author, title) values (nextval('book_seq'),'Adam Mickiewicz','Dziady');
+insert into Book (id, author, title) values (nextval('book_seq'),'J.K Rowling','Harry Potter');
+insert into Book (id, author, title) values (nextval('book_seq'),'Adam Mickiewicz','Pan Tadeusz');
+
+commit;
